dd9e1bf66a379b5203cef95cde7d02380c7f9cf8
Amit Kumar Hota <b123013@iiit-bh.ac.in>
2025-07-17 16:02:30 +0000

    Database Connection

--- backend_vault/src/db.rs
+++ backend_vault/src/db.rs
@@ -1,5 +1,7 @@
-use sqlx::{Pool, Postgres};
-use sqlx::query_as;
+use async_trait::async_trait;
+use chrono::{DateTime, Utc};
+use sqlx::{query_as, Error, Pool, Postgres};
+
 use crate::dtos::Capsule;
 #[derive(Debug, Clone)]
 pub struct DBClient {
@@ -10,15 +12,58 @@ impl DBClient {
     pub fn new(pool: Pool<Postgres>) -> Self {
         DBClient { pool }
     }
-
-    pub async fn get_capsules(&self) -> Result<Vec<Capsule>, sqlx::Error> {
-        let capsules = query_as!(
-            Capsule,
-            "SELECT * FROM capsules"
-        )
-        .fetch_all(&self.pool)
-        .await?;
-
-        Ok(capsules)
-    }
 }
+
+#[async_trait]
+pub trait TableExt {
+    async fn create_capsule(
+        &self,
+        name: &str,
+        email: &str,
+        title : &str,
+        message : &str,
+        unlock_at:DateTime<Utc>,
+        public_id:&str)-> Result<Capsule,Error>;
+
+    async fn get_all_capsule(&self) -> Result<Vec<Capsule>,Error>;
+
+    async fn get_by_pub_id(&self, public_id:&str) -> Result<Option<Vec<Capsule>>, Error>;
+
+}
+#[async_trait]
+impl TableExt for DBClient{
+
+   async fn create_capsule(
+        &self,
+        name: &str,
+        email: &str,
+        title : &str,
+        message : &str,
+        unlock_at:DateTime<Utc>,
+        public_id:&str)-> Result<Capsule,Error>{
+
+
+        }
+
+    async fn get_all_capsule(&self) -> Result<Vec<Capsule>,Error>{
+
+    }
+
+    async fn get_by_pub_id(&self, public_id:&str) -> Result<Option<Vec<Capsule>>, Error>{
+        let row = query_as!(
+            Capsule,
+            r#"
+            INSERT INTO capsules (public_id, name ,email, title , message, unlock_at) 
+            VALUES($1,$2, $3, $4, $5, $6)
+
+            RETURNING id, public_id, name, email, title ,message, unlock_at, created_at, is_unlocked"#,
+            public_id, name, email, title, message,unlock_at
+        )
+        .fetch_one(&self.pool)
+        .await?;
+        Ok(row);
+    }
+
+}
+    
+ 

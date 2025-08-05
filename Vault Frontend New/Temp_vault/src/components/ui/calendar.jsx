fde6bca72b452d9918722b68d41652824e851b52
Amit Kumar Hota <b123013@iiit-bh.ac.in>
2025-08-05 06:29:03 +0000

    calendar

--- Vault Frontend New/Temp_vault/src/components/ui/calendar.jsx
+++ Vault Frontend New/Temp_vault/src/components/ui/calendar.jsx
@@ -3,6 +3,7 @@ import { ChevronLeft, ChevronRight } from "lucide-react"
 import { DayPicker } from "react-day-picker"
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
+import "react-day-picker/dist/style.css"
 
 function Calendar({
   className,

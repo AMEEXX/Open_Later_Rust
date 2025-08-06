42cf4baec57937c3a3b7bc0a9a713bf8a22c599f
Amit Kumar Hota <b123013@iiit-bh.ac.in>
2025-08-06 08:17:02 +0000

    Update README

--- backend_vault/src/config.rs
+++ backend_vault/src/config.rs
@@ -3,13 +3,22 @@ pub struct Config{
     pub database_url: String,
     pub port : u16,
 }
+
 impl Config{
-    pub fn init() ->Config{
-        let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-        let port = std::env::var("PORT").expect("Port in not set in the .env").parse::<u16>().expect("The port must be a valid number");
+    pub fn init() -> Config{
+        let database_url = std::env::var("DATABASE_URL")
+            .expect("DATABASE_URL must be set");
+        
+        let port = std::env::var("PORT")
+            .unwrap_or_else(|_| "4000".to_string())
+            .parse::<u16>()
+            .expect("PORT must be a valid number");
+        
+        println!("Starting server on port: {}", port);
+        
         Config{
             database_url,
-            port ,
+            port,
         }
     }
 }

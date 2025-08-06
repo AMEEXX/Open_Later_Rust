5522b298cf8c06a332739d6cc222029360ae9d65
Amit Kumar Hota <b123013@iiit-bh.ac.in>
2025-08-06 08:44:44 +0000

    api.js changes for commit

--- Vault Frontend New/Temp_vault/src/lib/api.js
+++ Vault Frontend New/Temp_vault/src/lib/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:4000"; // change to your backend
+
+const API_BASE = "https://open-later-rust-backend.onrender.com";
+
 
 export const getAllCapsules = async () => {
   const res = await axios.get(`${API_BASE}/capsules`, {
@@ -13,11 +15,15 @@ export const getAllCapsules = async () => {
 };
 
 export const getCapsuleByPublicId = async (public_id) => {
-  const res = await axios.get(`${API_BASE}/capsule/${public_id}`);
+  const res = await axios.get(`${API_BASE}/capsule/${public_id}`, {
+    withCredentials: true,
+  });
   return res.data;
 };
 
 export const createCapsule = async (data) => {
-  const res = await axios.post(`${API_BASE}/create`, data);
+  const res = await axios.post(`${API_BASE}/create`, data, {
+    withCredentials: true,
+  });
   return res.data;
 };
